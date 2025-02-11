405976b5629b982ecc2a08f9f47e8cfecd84d1d3
jangsen <dh7895210@naver.com>
2025-02-11 15:53:06 +0900

    Update order.module.ts

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -4,49 +4,56 @@ import { OrderController } from './order.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Order]),
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: 'BOOK_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: [
-            'amqps://yytralag:zE6Ujbm5XE2yTjui7OjqXOiCJ0pP_r_i@fuji.lmq.cloudamqp.com/yytralag',
-          ],
-          queue: 'book_queue',
-          queueOptions: {
-            durable: false,
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_URL')],
+            queue: 'book_queue',
+            queueOptions: {
+              durable: false,
+            },
           },
-        },
+        }),
       },
       {
         name: 'CUSTOMER_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: [
-            'amqps://yytralag:zE6Ujbm5XE2yTjui7OjqXOiCJ0pP_r_i@fuji.lmq.cloudamqp.com/yytralag',
-          ],
-          queue: 'customer_queue',
-          queueOptions: {
-            durable: false,
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_URL')],
+            queue: 'customer_queue',
+            queueOptions: {
+              durable: false,
+            },
           },
-        },
+        }),
       },
       {
         name: 'ORDER_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: [
-            'amqps://yytralag:zE6Ujbm5XE2yTjui7OjqXOiCJ0pP_r_i@fuji.lmq.cloudamqp.com/yytralag',
-          ],
-          queue: 'order_queue',
-          queueOptions: {
-            durable: false,
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_URL')],
+            queue: 'order_queue',
+            queueOptions: {
+              durable: false,
+            },
           },
-        },
+        }),
       },
     ]),
   ],
